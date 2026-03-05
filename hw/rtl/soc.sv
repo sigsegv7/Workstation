d65952a67741c0126c2a1f8a03a10454518eff7d
Ian Moffett <ian@mirocom.org>
2026-03-05 09:56:35 -0500

    hw: soc: Add 250 MHz root clock domain + reset bridge
    Signed-off-by: Ian Moffett <ian@mirocom.org>

--- hw/rtl/soc.sv
+++ hw/rtl/soc.sv
@@ -10,9 +10,29 @@
 // @reset_i:   Reset input
 //
 module soc (
-    /* verilator lint_off UNUSEDSIGNAL */
     input wire clk_i,
     input wire reset_i
 );
+    /* verilator lint_off UNUSEDSIGNAL */
+    logic clk;
+    logic reset;
+`ifndef WORKSTATION_SIM
+    logic pll_locked;
 
+    // PLL for 250 MHz root clock domain
+    pll root_pll (
+        .refclk(clk_i),
+        .rst(~reset_i),
+        .outclk_0(clk),
+        .locked(pll_locked)
+    );
+
+    // Reset bridge
+    always_ff @(posedge clk) begin
+        reset <= ~reset_i | ~pll_locked;
+    end
+`else
+    assign clk = clk_i;
+    assign reset = ~reset_i;
+`endif  /* !WORKSTATION_SIM */
 endmodule
